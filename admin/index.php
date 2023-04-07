4aa70ba5afb1c68738cff6b4900c5a693d2605ad
badassboy <emmanuelobeng7788@gmail.com>
2023-04-07 08:51:57 +0100

    added admin section

---
+++ admin/index.php
@@ -0,0 +1,79 @@
+<?php 
+
+require("../library.php");
+$bank =  new ChaseBank();
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="ChaseBank">
+    <meta name="author" content="chasebank">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>ChaseBank</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <!-- <link href="cover.css" rel="stylesheet"> -->
+  </head>
+
+  <body class="text-center">
+
+   
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Username</th>
+      <th scope="col">Password</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+     <?php 
+    $details = $bank->getUserDetails();
+
+  // loop through the array returned from the user details array
+    foreach ($details as $user) {
+    
+  ?>
+    <tr>
+      <th scope="row"><?php echo $user['id'];?></th>
+      <td><?php echo $user['username'];?></td>
+      <td><?php echo $user['password'];?></td>
+     
+    </tr>
+
+  <?php } ?>
+
+  
+
+   
+
+  </tbody>
+</table>
+
+
+   
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- library.php
+++ library.php
@@ -6,6 +6,12 @@ error_reporting(E_ALL);
 require("database.php");
 
 class ChaseBank {
+
+	// This function takes in two paramters, that's username and password.
+	//  This function is used to insert a new user record into a database table named "users".
+	// The function starts by creating a new instance of the PDO database connection object using the DB() function. This function is not defined in the code snippet you provided, so it is likely defined in a separate file. The PDO object is used to connect to the database.
+
+// The next line of code creates a prepared statement using the prepare() method of the PDO object. This statement uses placeholders (?) to represent the values that will be inserted into the table. The actual values are provided as an array in the execute() method that is called on the statement object. This is a secure way to execute SQL queries and helps prevent SQL injection attacks.
 	public function UserRegistration($username,$password)
 	{
 		$db = DB();
@@ -19,7 +25,21 @@ class ChaseBank {
 			return false;
 		}
 	}
+
+	// This function select all user details from the database
+	public function getUserDetails()
+	{
+		$db = DB();
+		$stmt = $db->prepare('SELECT * FROM users');
+        $stmt->execute();
+        $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $user;
+
+
+	}
 }
+
+
 
 
 ?>
